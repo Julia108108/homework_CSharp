cdd3b407b86ad34db6c6c2ab120af210a3586e50
Julia <matterfixed@gmail.com>
2023-01-27 15:33:25 +0300

    rename task3

---
+++ hw1_task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+System.Console.Write ("Введите число и вы увидете является ли оно чётным: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine ("Да");
+}
+else
+{
+    Console.WriteLine ("Нет");
+}
+
+System.Console.WriteLine(" ");
