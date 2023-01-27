d8189b54aa7238f7e76252c5172e5241f7fa46e4
Julia <matterfixed@gmail.com>
2023-01-27 15:31:06 +0300

    rename task1

---
+++ hw1_task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+System.Console.Write("First figure: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Second figure: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    System.Console.WriteLine ("a");
+}
+
+else
+{
+    System.Console.WriteLine ($"{b}");
+}
