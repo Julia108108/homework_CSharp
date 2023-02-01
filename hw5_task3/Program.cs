21e6e826455aa8b53afa41ed0913b6fffc0bc5c7
Julia <matterfixed@gmail.com>
2023-02-02 00:17:10 +0300

    in process

--- hw5_task3/Program.cs
+++ hw5_task3/Program.cs
@@ -1,15 +1,44 @@
 ﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между
 // максимальным и минимальным элементов массива.
 
-System.Console.Write("Размер массива --> ");
-int len = Convert.ToInt32(Console.ReadLine());
-double[] array = new double[len];
-Random rnd = new Random();
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
 
-FillRandomArray(array, len);
-PrintArray(array, len);
-ArrayMinMax(array, out double min, out double max);
-Console.WriteLine($ Max element; {max} min element: {min}"");
-Console.WriteLine($"Разница между макс и минэл: {max-min}");
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
+        {
+            max = numbers[z];
+        }
+    if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
+}
 
-на тел продолжение кода
+Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
