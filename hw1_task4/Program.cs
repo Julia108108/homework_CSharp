b16d8b29496937747e1e582d57a8a57c90922506
Julia <matterfixed@gmail.com>
2023-02-01 17:06:31 +0300

    done

--- hw1_task4/Program.cs
+++ hw1_task4/Program.cs
@@ -1,18 +1,19 @@
-﻿// Напишите программу, которая на вход принимает число (N), 
+﻿// Задача 8. Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
 int i = 1;
 
-for (i < a)
-{
-    if (i % 2 ==0)
-    
-    Console.WriteLine(i);
-    i++;
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
-}
-   
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+    if (i % 2 !=1)
+    {
+        Console.Write(i + " ");    
+    }
+    i++;  
+} 
