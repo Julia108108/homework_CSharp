6a95231e89d2decb803b5d9299a853490105293d
Julia <matterfixed@gmail.com>
2023-02-01 17:10:32 +0300

    done

--- hw1_task1/Program.cs
+++ hw1_task1/Program.cs
@@ -1,10 +1,9 @@
-﻿// Напишите программу, которая на вход принимает два числа
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа
 // и выдаёт, какое число большее, а какое меньшее.
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Clear();
 System.Console.Write("First figure: ");
 int a = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Second figure: ");
