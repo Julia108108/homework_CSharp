d658e3c78210ade1960093705b0e1779c6c70397
Julia <matterfixed@gmail.com>
2023-02-01 22:52:24 +0300

    done

--- hw4_task3/Program.cs
+++ hw4_task3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29. Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// сделать массив со случайными элементами или можно со вводом
+
+Console.Write("--> ");
+int[] array = new int [8];
+
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100); 
+    Console.WriteLine(array[i]);
+}
