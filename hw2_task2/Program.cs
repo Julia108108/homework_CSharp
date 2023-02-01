dbba9221176d79c29dcf0d00875beb20cc8a47e5
Julia <matterfixed@gmail.com>
2023-02-01 20:13:29 +0300

    done

--- hw2_task2/Program.cs
+++ hw2_task2/Program.cs
@@ -1,35 +1,14 @@
-﻿// Напишите программу, которая выводит третью цифру
+﻿// Задача 13. Напишите программу, которая выводит третью цифру
 // заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-//не знаем длину числа.
-//можно математически оперировать числами
-//через сроки также можно решить и не конвертировать. это проще
+int n = 277896;
 
-//System.Console.Write ("Введите число: ");
+while (n > 999)
+{
+    n = n / 10;
+}
 
-Random rnd = new Random();
-int a = rnd.Next();
-
-System.Console.WriteLine($"Число {a}");
-
-int firstNumber = a / 100;
-int lastNumber = a % 10;
-
-a = firstNumber * 10 + lastNumber;
-
-System.Console.WriteLine (a);
-
-//if (a < 100)
-//{
-//    System.Console.WriteLine("Увы, в числе отсутствует третья цифра");
-//}
-
-//else
-//{
-//    int b = a % 10;
-//    System.Console.WriteLine (b);
-//}
-
+System.Console.WriteLine (n % 10);
