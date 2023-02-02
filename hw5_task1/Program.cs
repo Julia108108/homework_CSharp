50ca2bab90ecbbb071513793a9c42756fd97b1c3
Julia <matterfixed@gmail.com>
2023-02-02 11:30:43 +0300

    in process

--- hw5_task1/Program.cs
+++ hw5_task1/Program.cs
@@ -2,37 +2,16 @@
 // числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-//Console.Write("--> ");
-int[] array = new int [4];
-
-//Random rand = new Random();
-
-//for (int i = 0; i < array.Length; i++)
-//{
-//    array[i] = rand.Next(1000); 
-//    Console.WriteLine(array[i]);
-//}
-
-//if ([i] % 2 == 0)
-//{
-//    Console.WriteLine ("Четные числа: ");
-//}
-//else
-//{
-//    Console.WriteLine ("Нет");
-//}
-
-
-Console.WriteLine("Введите размер массива:  ");
+Console.WriteLine("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("массив: ");
+Console.WriteLine("Массив: ");
 PrintArray(numbers);
 int count = 0;
 
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
+for (int a = 0; a < numbers.Length; a++)
+if (numbers[a] % 2 == 0)
 count++;
 
 Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
