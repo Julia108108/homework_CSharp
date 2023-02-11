eab76ea5d6aac55870c535c2a8d28ff9cceaf0a4
Julia <matterfixed@gmail.com>
2023-02-11 16:44:32 +0300

    done

--- hw8_task1/Program.cs
+++ hw8_task1/Program.cs
@@ -1,33 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
 
 //отсортировать каждую строчку
 
+Console.WriteLine("Напишите количестов строк двумерного массива: ");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Напишите количестов столбцов двумерного массива: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int[,] table = new int[m, n];
+FillArrayRandom(table);
+PrintArray(table);
+SortToLower(table);
+Console.WriteLine();
+PrintArray(table);
+//Random rand = new Random();
 
-Console.Write("Введите строку:");
-int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
-Console.Write("Введите столбец:");
-int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-int n = 5; // размер массива
-int m = 7; // размер массива
-Random random = new Random();
-int[,] arr = new int[n, m];
-Console.WriteLine("Исходный массив:");
-for (int i = 0; i < arr.GetLength(0); i++)
+// Функция заполнения массива рандомно числами от 1 до 9
+void FillArrayRandom(int[,] array)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++) // i - строки
     {
-        arr[i, j] = random.Next(10, 99);
-        Console.Write("{0} ", arr[i, j]);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Функция сортировки элементов в строке двумерного массива от мин к макс
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int a = 0; a < array.GetLength(1) - 1; a++)
+            {
+                if(array[i, a] < array[i, a +1])
+                {
+                    int temp = array[i, a + 1];
+                    array[i, a + 1] = array[i, a];
+                    array[i, a] = temp;
+                }
+            }
+        }   
+    }
+}
+
+// Функция вывода двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
     }
     Console.WriteLine();
 }
-if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
-{
-    Console.WriteLine("Элемент не существует");
-}
-else
-{
-    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
-}
-Console.ReadLine();
- 
+
