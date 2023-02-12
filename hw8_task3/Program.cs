d2ed3008803db38b500a97e333e34ca57504f047
Julia <matterfixed@gmail.com>
2023-02-12 22:43:46 +0300

    done

---
+++ hw8_task3/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+int[,] arrayFirst = new int[2, 2];
+int[,] arraySecond = new int[2, 2];
+FillArrayByRandom(arrayFirst);
+FillArrayByRandom(arraySecond);
+Print(arrayFirst);
+Print(arraySecond);
+Multiply(arrayFirst, arraySecond);
+//Print(Multiply(arrayFirst, arraySecond));
+
+
+
+
+
+void FillArrayByRandom(int[,] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++) //
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void Print(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) //
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j] +  " "}"); //выводит
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int Multiply(int[,] arrayFirst, int[,] arraySecond);
+if (arrayFirst.GetLength(0) != arraySecond.GetLength(1))
+{
+    Console.WriteLine("Подобные матрицы нельзя перемножить");
+    return;
+}
+for (int i = 0; i < arrayFirst.GetLength(0); i++)
+{
+    for (int j = 0; j < arraySecond.GetLength(1); j++)
+    {
+        Multiply[i, j] = 0;
+        for (int k = 0; k < arrayFirst.GetLength(1); k++)
+        {
+            Multiply[i, j] = Multiply + arrayFirst[i, k] * arraySecond[k, j];
+            return;
+        }
+    }
+    return;
+}
