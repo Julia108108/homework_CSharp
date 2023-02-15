b241d30fc76fd95844d0f6e40f7fb1a428fae522
Julia <matterfixed@gmail.com>
2023-02-15 13:52:55 +0300

    done

--- hw9_task2/Program.cs
+++ hw9_task2/Program.cs
@@ -3,3 +3,27 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.WriteLine("Задайте значение M и N и программа выведет сумму натуральных элементов в промежутке от M до N");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = CallculateSumOfRange(m, n);
+
+Console.WriteLine($"а вот и ваша сумма {sum}");
+
+
+int CallculateSumOfRange(int m, int n)
+{
+    if (m < n)
+    {
+        return m + CallculateSumOfRange(m + 1, n);
+    }
+    else if (n > m)
+    {
+        return m + CallculateSumOfRange(m - 1, n);
+    }
+    else
+    {
+        return m;
+    }
+}
