71589c2867c9c37751e5a7b71250da775ead507c
Julia <matterfixed@gmail.com>
2023-02-15 13:52:38 +0300

    done

--- hw9_task1/Program.cs
+++ hw9_task1/Program.cs
@@ -2,3 +2,19 @@
 // натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Задайте значение N и программа выведет все натуральные числа в промежутке от N до 1.");
+int n = Convert.ToInt32 (Console.ReadLine());
+
+WritePositiveIntsDesc(n);
+
+ 
+ void WritePositiveIntsDesc (int n)
+ {
+    Console.Write(n + ", ");
+    if (n == 1 || n < 0)
+    {
+        return;
+    }
+    WritePositiveIntsDesc(n - 1);
+ }
